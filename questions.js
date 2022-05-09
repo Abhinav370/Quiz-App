58ef0b5df191f2e689c2117f5c879178ce2a4d7c
Abhinav Arora <70691885+Abhinav370@users.noreply.github.com>
2022-05-09 17:56:19 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,113 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+    numb: 6,
+    question: "What does OOPS stands for?",
+    answer: "Object Oriented Programming Language",
+    options: [
+      "Object acessible Language",
+      "Object Manipulative Language",
+      "Object Oriented Programming Language",
+      "Object Examination Programming Language"
+    ]
+},
+{
+    numb: 7,
+    question: "Which is the latest version of HTML?",
+    answer: "HTML5",
+    options: [
+        "HTML8",
+        "HTML2",
+        "HTML7",
+        "HTML5"
+    ]
+},
+{
+    numb: 8,
+    question: "Which option is having correct Keywords that are used to declare variables in JavaScript?",
+    answer: "var, let, const",
+    options: [
+        "int, char, bool",
+        "contains all Primitive and Non-Primitive data types",
+        "float, byte, double",
+        "var, let, const"
+    ]
+},
+{
+    numb: 9,
+    question: "Which of the following methods can be used to display data in some form using Javascript?",
+    answer: "All of the above",
+    options: [
+        "document.write()",
+        "window.alert()",
+        "console.log()",
+        "All of the above"
+    ]
+},
+{
+    numb: 10,
+    question: "What does the ‘toLocateString()’ method do in JS?",
+    answer: "Returns a localized string representation of an object",
+    options: [
+        "Returns a localised object representation",
+        "Returns a parsed string",
+        "Returns a localized string representation of an object",
+        "Returns Nothing"
+    ]
+},
+];
